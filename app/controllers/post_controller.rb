8ca1da8be4a81e108c4a53072abbb5f5a2153c3e
mizar <>
2020-01-26 16:46:12 +0900

    投稿機能追加

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -2,4 +2,23 @@ class PostController < ApplicationController
   def index
     @posts = Posted.all
   end
+
+  def new
+    @post = Posted.new
+  end
+
+  def create
+    post = Posted.new(post_params)
+    Posted.create(
+      name: post.name,
+      place: post.place,
+      content: post.content,
+      datetime: DateTime.now
+    )
+  end
+
+  private
+  def post_params
+      params.require(:posted).permit(:name, :content, :place)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'top#index'
-  resources :'post', only: [:index, :new]
+  resources :'post', only: [:index, :new, :create]
   #get 'post' => 'post#index'
   #get 'post' => 'post#new'
 end
