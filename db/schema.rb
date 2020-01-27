27c5181852a5996f12e45a886fa3ed19bf1f3807
watabe <hiroki.watabe@e-harp.jp>
2020-01-27 15:18:23 +0900

    db定義変更 (posted -> posteds)

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2020_01_19_083526) do
 
-  create_table "posted", force: :cascade do |t|
+  create_table "posteds", force: :cascade do |t|
     t.string "name"
     t.string "content"
     t.string "place"
